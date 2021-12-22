bfe548fd132d8ed9e28877b0f68ecca87cb9225c
Chirag Mehta <chiragmehta.uk@gmail.com>
2021-12-22 17:14:27 +0000

    deleted our solution code

--- main.js
+++ main.js
@@ -20,29 +20,8 @@ There is never more than 13 (X) so that's the max number you need to worry about
 
 
 //👉 Write the function your CodeWarriors will start with below here:
-// function isChristmasCancelled(str){
-//     //...
-//   }
-//-----------------------------
-
-
-export function isChristmasCancelled(str) {
-  const regex = /[Xx]/g;
-  let array = str.match(regex);
-  let count = array.length;
-  
-//   array.forEach((letter)=>{
-//     if (letter === "X") {
-//       count++
-//     }
-//   })
-
-  if (count < 4 && count > 0) {
-    return "No, Ho, Ho :( because Christmas has been cancelled...."
-  } else if (count > 4 && count <= 12) {
-    return "Santa Claus is coming... so still be good!"
-  } else if (count = 13) {
-    return "Merry Christmas, you can have a mulled wine!"
+function isChristmasCancelled(str){
+    //...
   }
-}
+
 
