92066121f45eb50f00047ef8ce4d1ee122a836a7
Chirag Mehta <chiragmehta.uk@gmail.com>
2021-12-22 15:46:18 +0000

    Kata plan to count santa �  created

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 /* 
 👉 Write your kata here!
+
+Is Christmas cancelled???
+
+Count up the santa emojis (🎅) in a given string. 
+
+If there's less than 4 (🎅) return 'No, Ho, Ho 😔 because Christmas has been cancelled....'
+
+If there's between 4 - 12 (🎅) return 'Santa Claus is coming... so still be good!'
+
+If there's 13 (🎅) return 'Merry Christmas, you can have a mulled wine 🎄🍷❄️'
+
+There is never more than 13 (🎅) so that's the max number you need to worry about.
+
+"T🎅his is our🎅 Christmas stri🎅ng" --> 'No, Ho, Ho 😔 because Christmas has been cancelled....'
+"T🎅🎅his🎅 is🎅 our🎅🎅🎅 Christ🎅mas stri🎅🎅ng" --> 'Santa Claus is coming... so still be good!';
+"🎅T🎅🎅his🎅 is🎅 our🎅🎅🎅 Christ🎅🎅mas st🎅ri🎅🎅ng" --> 'Merry Christmas, you can have a mulled wine 🎄🍷❄️';
 */
 
+
 //👉 Write the function your CodeWarriors will start with below here:
+function isChristmasCancelled(str){
+    //...
+  }
--- main.test.js
+++ main.test.js
@@ -1,1 +1,5 @@
 //👉 Write your tests below here:
+const testString = "T🎅🎅his🎅 is🎅 our🎅🎅🎅 Christ🎅mas stri🎅🎅ng"
+const testString = "T🎅his🎅 is🎅 our🎅🎅 Christ🎅mas stri🎅ng"
+const testString = "T🎅🎅🎅his🎅 is🎅🎅🎅 our🎅🎅 Christ🎅🎅🎅mas stri🎅ng"
+
