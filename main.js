aff647aa9933fbc593612023a86370bfcaa3cc81
sandraskolarczyk <spts20@sussex.ac.uk>
2021-12-23 10:48:13 +0000

    refactor instructions

--- main.js
+++ main.js
@@ -3,20 +3,27 @@
 
 Is Christmas cancelled???
 
+INSTRUCTIONS
 Count up the number of Xs in a given string. 
 
 If there's less than 4 (X) return 'No, Ho, Ho :( because Christmas has been cancelled....'
 
 If there's between 4 - 12 (X) return 'Santa Claus is coming... so still be good!'
 
-If there's 13 (X) return 'Merry Christmas, you can have a mulled wine'
+If there's 13 (X) return 'Merry Christmas, you can have a mulled wine!'
 
 There is never more than 13 (X) so that's the max number you need to worry about.
 
-"TXhis is ourX Christmas striXng" --> 'No, Ho, Ho :( because Christmas has been cancelled....'
-"TXXhisX isX ourXXX ChristXmas striXXng" --> 'Santa Claus is coming... so still be good!';
-"XTXXhisX isX ourXXX ChristXXmas stXriXXng" --> 'Merry Christmas, you can have a mulled wine';
+*BEWARE* 
+There are Santa emojis hidden in the string so you might want to think about removing them first and then count the number of (X)...
+In the spirit of Christmas, we will give you a hint ---> you might want to take a look at match() array method 😉 
+
+EXAMPLE
+"TXh🎅is is ou🎅rX Christmas s🎅triXng" --> 'No, Ho, Ho :( because Christmas has been cancelled....'
+"TXXh🎅isX isX ourX🎅🎅XX Chr🎅istXmas striXXng" --> 'Santa Claus is coming... so still be good!';
+"X🎅TX🎅🎅XhisX🎅 isX🎅 ourXXX🎅 ChristXXmas stXr🎅iXXng" --> 'Merry Christmas, you can have a mulled wine';
 */
+
 
 
 //👉 Write the function your CodeWarriors will start with below here:
